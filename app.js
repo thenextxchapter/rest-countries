524c34c8ca0be0d5ea4de34049f05253b1b6f894
chinonso ata <atac@reimnet.com>
2020-11-08 01:21:16 +0100

    Connection to Cluster

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const mongo = require("mongo");
+const MongoClient = mongo.MongoClient;
+const URI =
+  "mongodb+srv://Nony:redcarpet@cluster0.fe0md.mongodb.net/rest_countries?retryWrites=true&w=majority";
+// var client;
 const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require("cors");
@@ -23,6 +28,20 @@ app.use(bodyParser.json());
 
 /** CORS Middleware */
 app.use(cors());
+
+var mongoClient = new MongoClient(URI, {
+  reconnectTries: Number.MAX_VALUE,
+  autoReconnect: true,
+  useNewUrlParser: true,
+});
+mongoClient.connect((err, db) => {
+  // returns db connection
+  if (err != null) {
+    console.log(err);
+    return;
+  }
+  client = db;
+});
 
 //  Apply Path Modules
 //  Setting up the static directory
